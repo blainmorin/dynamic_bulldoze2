186df0be46e4e2cd064876f7b3105b381f8eafd1
blainmorin <morin.42@osu.edu>
2022-02-01 11:12:08 -0500

    fix

--- model30.R
+++ model30.R
@@ -95,4 +95,4 @@ model = ctStanFit(datalong = dff,
                   nopriors = FALSE,
                   control = list(max_treedepth = 12, adapt_delta = .99))
 
-save(model, file = "model24")
+save(model, file = "model30")
--- model32.R
+++ model32.R
@@ -98,4 +98,4 @@ model = ctStanFit(datalong = dff,
                   nopriors = FALSE,
                   control = list(max_treedepth = 12, adapt_delta = .99))
 
-save(model, file = "model31")
+save(model, file = "model32")
