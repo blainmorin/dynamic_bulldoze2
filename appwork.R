e21c41472cbc14e631729393fb76777cbd47ad0c
blainmorin <morin.42@osu.edu>
2022-02-04 12:13:12 -0500

    save

--- appwork.R
+++ appwork.R
@@ -59,6 +59,9 @@ g = kalman.data %>%
   geom_line(aes(color = Subject, group = Element), data = d2) +
   facet_wrap(~Row, ncol = 1) +
   theme_bw() +
+  theme(strip.background =element_rect(fill="black")) +
+  theme(strip.text = element_text(colour = 'white')) +
+  theme(legend.position = "none") +
   ggtitle("Manifest Values") +
   ylab("") +
   xlab("Year")
@@ -70,3 +73,12 @@ g = g +
   geom_ribbon(aes(ymin = value - sd, ymax = value + sd,
                   color = Subject), linetype = "dotted",
               alpha = .2) 
+
+#####################################
+#####################################
+#####################################
+# Summary  Output
+
+check = summary(model)$parmatrice
+
+
