a25ed14639ec512c545eaea62465fb2d54fa851c
blainmorin <morin.42@osu.edu>
2022-02-09 01:23:10 -0500

    save

--- model40.R
+++ model40.R
@@ -88,7 +88,7 @@ model = ctModel(type='stanct',
 
 model = ctStanFit(datalong = dff,
                   ctstanmodel = model,
-                  iter = 6000,
+                  iter = 4000,
                   chains = 4,
                   cores = 4, 
                   optimize = FALSE,
